8a5837fddeeda20209f984bb1409be8c287ac93c
Алексей Миллер <miller.alex2004@gmail.com>
2024-06-01 23:31:55 +0700

    аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -2,5 +2,11 @@ function showTime() {
     let today = new Date();
     let currentTime = today.toLocaleTimeString('ru-RU');
     document.getElementById('time').innerHTML = currentTime;
+
+    let seconds = today.getSeconds();
+    let secondHand = document.getElementById('second-hand');
+    let secondAngle = seconds * 6; // 6 degrees per second
+    secondHand.style.transform = `translateX(-50%) rotate(${secondAngle}deg)`;
 }
+
 setInterval(showTime, 1000);
